157fb9d20dec4c987f979ed32ba548a84d915e84
TwistedVision518 <brawlclicks58@gmail.com>
2026-03-21 10:35:59 +0530

    Aerodactyl: Switch zram to zstd, enable dexpreopt, and turn on TCP Fast Open
    Move the virtual A/B selection logic over to PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE, enable full dexpreopt, switch zram compression from lz4 to zstd, and enable TCP Fast Open for IPv4 sockets.

--- device-common.mk
+++ device-common.mk
@@ -8,11 +8,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # A/B
-ifeq ($(WITH_GMS),true)
-PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE ?= erofs
+ifeq ($(PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE),ext4)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 else
-PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE ?= ext4
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 endif
+
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 
 ifeq ($(PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE),ext4)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
