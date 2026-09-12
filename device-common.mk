968d3917beb691b1b708361e04e1b0b897eaa9a8
darkdolphin1 <shravanrajapure1@gmail.com>
2026-09-12 15:42:12 +0530

    Aerodactyl: include hw/dolby and LunarisDolby
    requires hardware/dolby[1] and lunarisDolby[2]
    
    [1]: https://github.com/nyxalune/android_hardware_dolby (sony branch)
    [2]: https://github.com/nyxalune/packages_apps_DolbyUI
    Signed-off-by: darkdolphin1 <shravanrajapure1@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -131,6 +131,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Dolby Atmos
+$(call inherit-product, hardware/dolby/dolby.mk)
+
+PRODUCT_PACKAGES += \
+    DolbyAtmos
+
 # Doze
 PRODUCT_PACKAGES += \
     NothingDoze
