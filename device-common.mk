c7269d7ebaa77d2b7bcbb924865aebffe8668eb9
TwistedVision518 <pranavpraveen0@gmail.com>
2026-09-13 20:19:14 +0000

    Aerodactyl: audio: Integrate ViPER4Android FX
    - Inherit packages/apps/ViPER4AndroidFX/config.mk
    - Add v4a_re library and v4a_standard_re effect to audio_effects.xml
    - Add SELinux policies for audioserver and hal_audio_default

--- device-common.mk
+++ device-common.mk
@@ -67,6 +67,9 @@ PRODUCT_PACKAGES += \
     MtkInCallService \
     NothingDirac
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration_a2dp_offload_enable_cg_enable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
